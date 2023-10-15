57355523b29e572f3db2a84b7e69286bddc0bc71
Chabil Kansal <chabilkansal@gmail.com>
2023-10-15 19:45:12 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t chabil/multi-client:latest -t chabil/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t chabil/multi-server:latest -t chabil/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t chabil/multi-worker:latest -t chabil/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push chabil/multi-client:latest
+docker push chabil/multi-server:latest
+docker push chabil/multi-worker:latest
+
+docker push chabil/multi-client:$SHA
+docker push chabil/multi-server:$SHA
+docker push chabil/multi-worker:$SHA
+
+kubectl apply -f k8s
+kuebctl set image deployments/server-deployment server=chabil/multi-server:$SHA
+kubectl set image deployments/client-deployment client=chabil/multi_client:$SHA
+kubectl set image deployments/worker-deployment worker=chabil/multi_worker:$SHA
